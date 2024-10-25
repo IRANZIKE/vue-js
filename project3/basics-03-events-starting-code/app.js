4986d7ab012091e60f23acbe6270f9fd02f9b033
IRANZIKE <121245572+IRANZIKE@users.noreply.github.com>
2024-10-25 18:01:35 +0200

    working with evnts argument

--- project3/basics-03-events-starting-code/app.js
+++ project3/basics-03-events-starting-code/app.js
@@ -5,12 +5,12 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    add() {
-      this.counter = this.counter + 1
+    add(num) {
+      this.counter = this.counter + num
     },
 
-    remove() {
-      this.counter = this.counter - 1
+    remove(num) {
+      this.counter = this.counter - num
     }
 
   },
