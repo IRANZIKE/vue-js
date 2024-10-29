446a6689b2cba8403ab7de8c0df2cb1e9be224b3
IRANZIKE <121245572+IRANZIKE@users.noreply.github.com>
2024-10-29 12:50:52 +0200

    adding css class dynamically

--- project3/basics-10-styling-starting-setup/app.js
+++ project3/basics-10-styling-starting-setup/app.js
@@ -10,11 +10,11 @@ const app = Vue.createApp({
     methods: {
         boxSelected(box) {
             if (box === 'A') {
-                this.boxAselected = true;
+                this.boxAselected = !this.boxAselected ;
             } else if (box === 'B') {
-                this.boxBselected = true;
+                this.boxBselected = !this.boxBselected;
             } else if (box === 'C') {
-                this.boxCselected = true;
+                this.boxCselected = !this.boxCselected;
             }
         },
     }
